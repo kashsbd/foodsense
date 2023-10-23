9bbee380c284072acc3c4da3fbe12e7820bd9742
Addisu16 <71025422+Addisu16@users.noreply.github.com>
2023-10-23 15:44:22 -0500

    updated

--- screens/note/navigation/NoteStackComponent.js
+++ screens/note/navigation/NoteStackComponent.js
@@ -16,12 +16,12 @@ function NoteStackComponent() {
       <NoteStack.Screen
         name="Details"
         component={NoteDetails}
-        options={{ headerShown: true }}
+        options={{ title:"Note Details",headerShown: true }}
       />
       <NoteStack.Screen
         name="add-note"
         component={AddNote}
-        options={{ headerShown: true }}
+        options={{ title:"Add Note", headerShown: true }}
       />
     </NoteStack.Navigator>
   );
