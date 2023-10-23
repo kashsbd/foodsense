709542396458f29ddf4afe462734cc88ff771719
tengis4262 <e.tengis@gmail.com>
2023-10-23 14:23:02 -0500

    Added Image feature Display Date on FoodList Upgraded Styles for AddFood

--- screens/food/components/FoodListItem.js
+++ screens/food/components/FoodListItem.js
@@ -1,65 +1,28 @@
-import React, { useState } from "react";
 import { Alert } from "react-native";
-
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { deleteFood } from "../network";
 import useToken from "../../../hooks/useToken";
 
 const FoodListItem = ({ data, reload }) => {
   const { token } = useToken();
-  const { _id, name, origin, price } = data;
+  const { _id, name, origin, price, date, image } = data;
 
   const navigation = useNavigation();
 
-  //use this to check on computer browser otherwise comment this below code if you
-  // are using on mobile device
-  // const handleDeleteItem = async () => {
-  //   try {
-  //     const res = await deleteFood(_id, token);
-  //     if (res) {
-  //       Alert.alert("Food Successfully Deleted");
-  //       console.log("Item deleted");
-  //       reload();
-  //     } else {
-  //       console.log("Error deleting food");
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-
-  // mobile version 
   const handleDeleteItem = async () => {
-    Alert.alert(
-      "Delete Food",
-      "Are you sure you want to delete this food?",
-      [
-        {
-          text: "Cancel",
-          onPress: () => console.log("Cancel Pressed"),
-          style: "cancel",
-        },
-        {
-          text: "Delete",
-          onPress: async () => {
-            try {
-              const res = await deleteFood(_id, token);
-              if (res) {
-                Alert.alert("Food Successfully Deleted");
-                console.log("Item deleted");
-                reload();
-              } else {
-                console.log("error deleting food");
-              }
-            } catch (error) {
-              console.log(error);
-            }
-          },
-        },
-      ],
-      { cancelable: false }
-    );
+    try {
+      const res = await deleteFood(_id, token);
+      if (res) {
+        Alert.alert("Food Successfully Deleted");
+        console.log("Item deleted");
+        reload();
+      } else {
+        console.log("Error deleting food");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -69,8 +32,11 @@ const FoodListItem = ({ data, reload }) => {
         <Text style={styles.foodDescription}>
           {origin} - ${price}
         </Text>
+        <Text style={styles.dateText}>Date Posted: {date}</Text>
       </View>
-
+      <View style={styles.imageContainer}>
+        <Image source={{ uri: image }} style={styles.image} />
+      </View>
       <View style={styles.buttonsContainer}>
         <TouchableOpacity
           onPress={() => {
@@ -82,7 +48,7 @@ const FoodListItem = ({ data, reload }) => {
         </TouchableOpacity>
         <TouchableOpacity
           onPress={handleDeleteItem}
-          style={styles.deleteButton} // Apply the deleteButton style here
+          style={styles.deleteButton}
         >
           <Text style={styles.buttonText}>Delete</Text>
         </TouchableOpacity>
@@ -100,7 +66,7 @@ const styles = StyleSheet.create({
     elevation: 4,
   },
   titleContainer: {
-    flexDirection: "row",
+    flexDirection: "column",
     justifyContent: "space-between",
     alignItems: "center",
   },
@@ -112,6 +78,18 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: "gray",
   },
+  dateText: {
+    fontSize: 16,
+    color: "green",
+  },
+  imageContainer: {
+    alignItems: "center",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
   buttonsContainer: {
     marginTop: 10,
     flexDirection: "row",
@@ -119,7 +97,7 @@ const styles = StyleSheet.create({
   },
   button: {
     backgroundColor: "#5398DC",
-    padding: 10,
+    padding: 5,
     borderRadius: 5,
     flex: 1,
     margin: 5,
